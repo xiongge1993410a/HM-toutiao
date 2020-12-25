e4411993d242182911cffbf3b2414fba52a68a08
xiongge <692508978@qq.com>
2020-12-25 15:39:20 +0800

    04 项目初始化-rem适配及请求封装模块

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
     <van-cell title="单元格" value="内容" />
     <van-cell title="单元格" value="内容" label="描述信息" />
   </van-cell-group>
+  <div class="box"></div>
   </div>
 </template>
 
@@ -22,4 +23,10 @@ export default {
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -12,6 +12,9 @@ import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
 
+// 加载动态设置REM基准值
+import 'amfe-flexible'
+
 // 注册使用Vant组件库
 Vue.use(Vant)
 
